ebe3913f8371e5b1cf97ccf7897a735ec3d442e8
Koi <61448492+koi-7@users.noreply.github.com>
2025-10-21 22:29:03 +0900

    #14 Slack 通知機能を追加

--- weight_manager/slack.py
+++ weight_manager/slack.py
@@ -1,43 +1,28 @@
 # coding: utf-8
 
 
-import json
 import time
 
 import requests
 
+from .const import Const
+
 
 class Slack:
-    def __init__(self, channel_id, token):
-        self.__channel_id = channel_id
-        self.__token = token
+    def __init__(self, config_ini):
+        self.__channel_id = config_ini['Slack']['channel_id']
+        self.__token = config_ini['Slack']['token']
 
-    def notify(self, sio):
-        upload_url, file_id = self._get_upload_url_and_file_id(sio)
-        self._upload_file_content(upload_url, sio)
-        self._complete_upload(file_id)
-
-        time.sleep(1)
-
-    def _get_upload_url_and_file_id(self, sio):
-        url = 'https://slack.com/api/files.getUploadURLExternal'
+    def notify(self, mode_year_months, spread_sheet_file_key):
+        '''Slack での通知を行う'''
+        url = Const.Slack.API_URL
         headers = {'Authorization': f'Bearer {self.__token}'}
-        params = {'filename': 'file.png', 'length': len(sio.getvalue())}
+        message = \
+            Const.Slack.MONTHLY_MESSAGE_BASE.format(year=mode_year_months.year, month=mode_year_months.months[0]) if mode_year_months.mode == Const.Mode.MONTH \
+            else Const.Slack.YEARLY_MESSAGE_BASE.format(year=mode_year_months.year)
+        file_url = Const.Slack.SPREAD_SHEET_URL_BASE.format(file_key=spread_sheet_file_key)
+        text = f'{message}\n{file_url}'
+        data = {'channel': self.__channel_id, 'text': text}
 
-        response = requests.get(url, headers=headers, params=params)
-        response_json = response.json()
-
-        return response_json['upload_url'], response_json['file_id']
-
-    def _upload_file_content(self, upload_url, sio):
-        requests.post(url=upload_url, data=sio.getvalue())
-
-    def _complete_upload(self, file_id):
-        url = 'https://slack.com/api/files.completeUploadExternal'
-        headers = {'Authorization': f'Bearer {self.__token}', 'Content-Type': 'application/json'}
-        data = {
-            'files': [{'id': file_id, 'title': 'title'}],
-            'channel_id': self.__channel_id
-        }
-
-        requests.post(url, headers=headers, data=json.dumps(data))
+        requests.post(url=url, headers=headers, data=data)
+        time.sleep(1)
